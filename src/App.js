41d54e8c10986c1c87a1052d9aec8c087599d86c
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-10-28 16:12:08 -0400

    added some hardcoded data to show when first opening the to do app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+// rendering initial data as a guideline for the todo app
+const data = [
+  {
+    name: 'Brush Teeth',
+    id: 123,
+    completed: false
+  },
+  {
+    name: 'Take out Trash',
+    id: 124,
+    completed: false
+  },
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
